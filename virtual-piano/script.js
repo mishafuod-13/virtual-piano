534eeed3182b920ddefdd783376a159c550d1870
Mikhail Fedarau <mishafuod@gmail.com>
2021-03-22 05:45:14 +0300

    feat: add functions for inscriptions

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -116,7 +116,6 @@ function clickKey (EO)  { // base function for mouse events
 function setData (htmlcol) {    //writes data from html-collection (piano) to class objects
 
     htmlcol.forEach((item) =>  {
-        console.log (item.dataset)
 
         if (item.dataset) {
             let note = item.dataset.note;
@@ -142,7 +141,44 @@ function setData (htmlcol) {    //writes data from html-collection (piano) to cl
             document.exitFullscreen();
             return;
         }
+}
+
+function inscripter (changelem) {
+    const butNot = document.querySelector(".btn-notes");
+    const butLet =  document.querySelector(".btn-letters");
+    const keyssharp = document.querySelector(".keys-sharp");
+
+    if (changelem === butLet) {
+        keyssharp.classList.add("piano-letter");
+        piano.classList.add("piano-letter");
+        butNot.classList.remove("btn-active");
+
+    }  else {
+        keyssharp.classList.remove("piano-letter");
+        piano.classList.remove("piano-letter");
+        butLet.classList.remove("btn-active");
+    } 
+
+}
+
+
+function changeInscript (EO) {
+    EO=EO||window.event;       
+    let button = EO.target||EO.srcElement;
+    const butInscript = document.querySelectorAll(".btn");
+    console.log (button)
+    if (button === butWrap){
+        return
     }
+    if (!button.classList.contains('btn-active')){
+        button.classList.add ("btn-active");
+        inscripter(button);
+    }
+    
+    
+}
+
+
     
 
 
@@ -153,9 +189,15 @@ const pianoKeys = document.querySelectorAll(".piano-key");
 
 const piano = document.querySelector(".piano");
 
+const butFull = document.querySelector(".fullscreen");
+
+const butWrap = document.querySelector(".btn-container");
+
 piano.addEventListener('mousedown',clickKey,false);
 
-const butFull = document.querySelector(".fullscreen");
+butWrap.addEventListener("click", changeInscript, false);
+
+
 
 
 
